2b4220a828b83b2aaf99b2e916460f11d4f06f69
tayyab-ilyas <tayyabilyas963@gmail.com>
2023-11-30 02:07:14 +0530

    random fortune

--- app.js
+++ app.js
@@ -7,7 +7,12 @@ app.engine("handlebars", engine());
 app.set("view engine", "handlebars");
 
 app.get("/", (req, res) => res.render("home"));
-app.get("/about", (req, res) => res.render("about"));
+
+const fortunes = ["quote1", "quote2", "quote3", "quote4", "quote5"];
+app.get("/about", (req, res) => {
+  const randomFortune = fortunes[Math.floor(Math.random() * fortunes.length)];
+  res.render("about", { fortune: randomFortune });
+});
 
 app.use((req, res) => {
   res.status(404);
