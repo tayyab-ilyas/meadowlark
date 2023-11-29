df7bbb9321e66cc24590837f15db7d10437c8dac
tayyab-ilyas <tayyabilyas963@gmail.com>
2023-11-30 02:22:58 +0530

    init jest

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
 const express = require("express");
 const { engine } = require("express-handlebars");
+const fortunes = require("./lib/fortune");
 const app = express();
+
 const port = 5000;
 
+app.use(express.static(__dirname + "/public"));
 app.engine("handlebars", engine());
 app.set("view engine", "handlebars");
 
 app.get("/", (req, res) => res.render("home"));
 
-const fortunes = ["quote1", "quote2", "quote3", "quote4", "quote5"];
 app.get("/about", (req, res) => {
   const randomFortune = fortunes[Math.floor(Math.random() * fortunes.length)];
   res.render("about", { fortune: randomFortune });
